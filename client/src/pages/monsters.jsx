72aa9fbfb36e08bba28ff6d27dd65f632c9d0860
Bjpippenger <144498503+Bjpippenger@users.noreply.github.com>
2024-03-13 01:37:37 -0700

    updated monsters and charactersheet

--- client/src/pages/monsters.jsx
+++ client/src/pages/monsters.jsx
@@ -1,31 +1,64 @@
-import { useState } from "react";
-//import reactLogo from "./assets/react.svg";
-//import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 //import "./App.css";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'; 
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
 
-const API_KEY = "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";
+let API_KEY;
 
 function Monsters() {
   const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
-      message: "Can you describe the monster you would like to create?",
+      message:
+        "Can you please describe the encounter you would like to create?",
       sender: "ChatGPT",
       direction: "incoming",
-    }
+    },
   ]);
+
+  useEffect(() => {
+    fetch("/api/openai-api-key", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log(res);
+          throw new Error("Failed to get OpenAI API key");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        API_KEY = data;
+      })
+      .catch((error) => {
+        console.error("Error getting OpenAI API key:", error);
+      });
+  }, []);
 
   const handleSend = async (message) => {
     const newMessage = {
       message: message,
       sender: "user",
-      direction: "outgoing"
+      direction: "outgoing",
     };
 
     const newMessages = [...messages, newMessage];
-    
+
     setMessages(newMessages);
 
     setTyping(true);
@@ -34,53 +67,52 @@ function Monsters() {
   };
 
   async function processMessageToChatGPT(chatMessages, retryDelay = 1000) {
-    
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
 
-      if(messageObject.sender === "ChatGPT") {
-        
+      if (messageObject.sender === "ChatGPT") {
         role = "assistant";
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message }
+      return { role: role, content: messageObject.message };
     });
 
     const systemMessage = {
       role: "system",
-      content: "Speak like you are a master D&D player that creates character sheets for your friends."
-    }
+      content:
+        "Speak like you are a master D&D player that creates character sheets for your friends.",
+    };
 
     const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        systemMessage,
-        ...apiMessages
-      ]
-    }
-    
-   await fetch("https://api.openai.com/v1/chat/completions", {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         Authorization: "Bearer " + API_KEY,
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(apiRequestBody)
-      }).then((data) => {
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
         return data.json();
-      }).then((data) => {
+      })
+      .then((data) => {
         console.log(data);
         console.log(data.choices[0].message.content);
-        setMessages(
-          [...chatMessages, {
+        setMessages([
+          ...chatMessages,
+          {
             message: data.choices[0].message.content,
-            sender: "ChatGPT"
-          }]
-        );
+            sender: "ChatGPT",
+          },
+        ]);
         setTyping(false);
-      })
-    }
+      });
+  }
 
   return (
     <div className="App">
@@ -88,7 +120,11 @@ function Monsters() {
         <MainContainer>
           <ChatContainer>
             <MessageList
-              typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing..." /> : null}
+              typingIndicator={
+                typing ? (
+                  <TypingIndicator content="ChatGPT is typing..." />
+                ) : null
+              }
             >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />;
